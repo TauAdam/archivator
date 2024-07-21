79829ab19b54da6b8473368f0596c88fb6491ee8
TauAdam <101412464+TauAdam@users.noreply.github.com>
2024-07-21 21:41:35 +0500

    feat: build encoded file bytes

--- lib/compress/vlc/vlc.go
+++ lib/compress/vlc/vlc.go
@@ -1,7 +1,11 @@
 package vlc
 
 import (
+	"bytes"
+	"encoding/binary"
+	"encoding/gob"
 	"github.com/TauAdam/archivator/lib/compress/vlc/table"
+	"log"
 	"strings"
 )
 
@@ -25,6 +29,33 @@ func (ed EncoderDecoder) Encode(str string) []byte {
 	chunks := splitByChunks(binStr, chunkSize)
 
 	return chunks.Bytes()
+}
+
+func buildEncodedFile(tbl table.EncodingTable, data string) []byte {
+	encodedTable := encodeTable(tbl)
+
+	var buf bytes.Buffer
+	buf.Write(encodeNumbers(len(encodedTable)))
+	buf.Write(encodeNumbers(len(tbl)))
+	buf.Write(encodedTable)
+	buf.Write(splitByChunks(data, chunkSize).Bytes())
+
+	return buf.Bytes()
+}
+
+func encodeTable(tbl table.EncodingTable) []byte {
+	var buf bytes.Buffer
+	err := gob.NewEncoder(&buf).Encode(tbl)
+	if err != nil {
+		log.Fatalf("serialization error: %v", err)
+	}
+	return buf.Bytes()
+}
+func encodeNumbers(num int) []byte {
+	res := make([]byte, 4)
+	binary.BigEndian.PutUint32(res, uint32(num))
+
+	return res
 }
 
 // Decode decodes the input bytes from VLC
